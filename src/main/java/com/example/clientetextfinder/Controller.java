b3e99b1708aa5e7e750df38e07e2e7f138cc6cec
MauroCR52 <mauluna52@gmail.com>
2022-10-24 22:11:47 -0600

    Implementacion de Cliente con servidor

---
+++ src/main/java/com/example/clientetextfinder/Controller.java
@@ -0,0 +1,94 @@
+package com.example.clientetextfinder;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private ScrollPane ScrollMain;
+    @FXML
+    private Button enviarButton;
+    @FXML
+    private TextField mensajeText;
+    @FXML
+    private VBox mensajeVBOX;
+    private Cliente cliente;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            cliente = new Cliente(new Socket("localhost", 3421));
+            System.out.println("Conectado a servidor");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mensajeVBOX.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+                ScrollMain.setVvalue((Double) newValue);
+            }
+        });
+
+        cliente.receiveMessageFromServer(mensajeVBOX);
+        enviarButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String messageToSend = mensajeText.getText();
+                if (!messageToSend.isEmpty()) {
+                    HBox hBox = new HBox();
+                    hBox.setAlignment(Pos.CENTER_RIGHT);
+                    hBox.setPadding(new Insets(5, 5, 5, 10));
+                    Text text = new Text(messageToSend);
+                    TextFlow textFlow = new TextFlow(text);
+                    textFlow.setStyle("-fx-color: rgb(239,242,255); " +
+                            "-fx-background-color: rgb(15,125,242);" +
+                            "-fx-background-radius: 20px");
+                    textFlow.setPadding(new Insets(5, 10, 5, 10));
+                    text.setFill(Color.color(0.934, 0.945, 0.996));
+                    hBox.getChildren().add(textFlow);
+                    mensajeVBOX.getChildren().add(hBox);
+                    cliente.sendMessageToServer(messageToSend);
+                    mensajeText.clear();
+                }
+            }
+        });
+    }
+
+    public static void addLabel(String msgFromServer, VBox vBox) {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5, 5, 5, 10));
+        Text text = new Text(msgFromServer);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-background-color: rgb(233,233,235);-fx-background-radius: 20px");
+        textFlow.setPadding(new Insets(5, 10, 5, 10));
+        hBox.getChildren().add(textFlow);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                vBox.getChildren().add(hBox);
+            }
+        });
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.clientetextfinder {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.clientetextfinder to javafx.fxml;
     exports com.example.clientetextfinder;
 }
